f3608f04893446a246aad6d963d32856f9c8b21a
unknown <xinjia.caoxin@gmail.com>
2021-05-04 17:52:43 -0400

    fixed scroll top form

--- src/pages/form.js
+++ src/pages/form.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import Form from '../components/Form';
+import ScrollToTop from '../components/ScrollToTop';
 
 const FormPage = () => {
     return (
         <div>
+            <ScrollToTop />
             <Form />
         </div>
     )
